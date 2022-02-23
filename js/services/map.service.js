535ca6302b81e8111efd85517e5987ace1395ad6
RZShachar <RZShachar@gmail.com>
2022-02-23 16:02:20 +0200

    Accepting fixed conflic

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -6,11 +6,7 @@ export const mapService = {
     panTo
 }
 
-<<<<<<< HEAD
 var gMap
-=======
-let gMap;
->>>>>>> aa284bf50a8d583150e1bd84d04893c66d9f2c2d
 
 function initMap(lat = 32.0749831, lng = 34.9120554) {
     console.log('InitMap')
@@ -32,40 +28,23 @@ function addMarker(loc) {
         map: gMap,
         title: 'Hello World!'
     })
-<<<<<<< HEAD
     return marker
 }
 
 function panTo(lat, lng) {
     var laLatLng = new google.maps.LatLng(lat, lng)
     gMap.panTo(laLatLng)
-=======
-    return marker;
-}
-
-function panTo(lat, lng) {
-    const laLatLng = new google.maps.LatLng(lat, lng);
-    gMap.panTo(laLatLng);
->>>>>>> aa284bf50a8d583150e1bd84d04893c66d9f2c2d
 }
 
 
 
 function _connectGoogleApi() {
     if (window.google) return Promise.resolve()
-<<<<<<< HEAD
     const API_KEY = '' //TODO: Enter your API Key
     var elGoogleApi = document.createElement('script')
     elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`
     elGoogleApi.async = true
     document.body.append(elGoogleApi)
-=======
-    const API_KEY = ''; //TODO: Enter your API Key
-    const elGoogleApi = document.createElement('script');
-    elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
-    elGoogleApi.async = true;
-    document.body.append(elGoogleApi);
->>>>>>> aa284bf50a8d583150e1bd84d04893c66d9f2c2d
 
     return new Promise((resolve, reject) => {
         elGoogleApi.onload = resolve
