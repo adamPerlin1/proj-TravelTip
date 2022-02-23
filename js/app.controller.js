79605f6440437d2cc0e310ca8dcbb063b1e77313
Adam <adampn462@gmail.com>
2022-02-23 22:03:27 +0200

    continued onSearchLoc

--- js/app.controller.js
+++ js/app.controller.js
@@ -76,6 +76,7 @@ function getPosition() {
 function onSearchLoc() {
     const value = document.querySelector('input').value
     locService.searchLoc(value)
+        .then(onPanTo)
 }
 
 function onAddMarker() {
